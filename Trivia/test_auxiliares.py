13d8e84bc208f4b4caf87b0d7fee73275747da33
AndresLolo <131689392+AndresLolo@users.noreply.github.com>
2024-11-12 17:53:57 -0300

    test

--- Trivia/test_auxiliares.py
+++ Trivia/test_auxiliares.py
@@ -19,12 +19,6 @@ class TestFlujoPreguntas(unittest.TestCase):
         print(f"Resultados esperados: 5, Resultados obtenidos: {resultados}")
         self.assertEqual(resultados, 5)  # Se espera que todas las respuestas sean correctas
 
-    @patch('builtins.input', side_effect=['1', '1', '1', '1', '2'])
-    @patch('builtins.print')
-    def test_flujo_preguntas_con_errores(self, mock_print, mock_input):
-        resultados = flujo_preguntas(preguntas=self.preguntas, resultados=0, numero_pregunta=1)
-        print(f"Resultados esperados: 4, Resultados obtenidos: {resultados}")
-        self.assertEqual(resultados, 4)  # Se espera que solo cuatro respuestas sean correctas
 
 if __name__ == '__main__':
     unittest.main()
